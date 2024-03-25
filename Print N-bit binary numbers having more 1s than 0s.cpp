1fdab7eaabd484b9852db3fa97d616444538f1b5
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-03-25 16:31:17 +0530

    Create Print N-bit binary numbers having more 1s than 0s.cpp

---
+++ Print N-bit binary numbers having more 1s than 0s.cpp
@@ -0,0 +1,61 @@
+```
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+    void solve(vector<string>&ans, string curr,int ones,int zeroes,int n){
+        if(ones<zeroes) return;
+        if(curr.size()==n){
+            ans.push_back(curr);
+            return;
+        }
+        solve(ans, curr+'1', ones+1, zeroes, n);
+        solve(ans, curr+'0', ones, zeroes+1, n);
+        return;
+    }
+	vector<string> NBitBinary(int n)
+	{
+	    // Your code goes here
+	    vector<string>ans;
+	    string curr="";
+	    solve(ans,curr,0,0,n);
+	    return ans;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+        Solution ob;
+   		vector<string> ans = ob.NBitBinary(n);
+
+   		for(auto i:ans)
+   			cout << i << " ";
+
+   		cout << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
+
+```
