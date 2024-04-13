8d825ac481bcacd8f91093fd3d8ad382bdfeed10
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-04-13 15:36:06 +0530

    Create 13 April  2024 Reverse Bits.cpp

---
+++ 13 April  2024 Reverse Bits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        // code here
+        long long ans = 0,i=31;
+        while(x){
+            ans+=(long long)pow(2,i)*(x&1);
+            i--;
+            x>>=1;
+        }
+        
+        return ans;
+            
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
