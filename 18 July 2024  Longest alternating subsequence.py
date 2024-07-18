7a31569d2cbed32749589e4f2d18f45d4a81af1d
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-07-18 14:34:11 +0530

    Create 18 July 2024  Longest alternating subsequence.py

---
+++ 18 July 2024  Longest alternating subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Function to find the maximum length of alternating subsequence
+    def alternatingMaxLength(self, arr):
+       #code here
+        if not arr:
+            return 0
+        up = 1
+        down = 1
+        
+        for i in range(1, len(arr)):
+            if arr[i] > arr[i - 1]:
+                up = down + 1
+            elif arr[i] < arr[i - 1]:
+                down = up + 1
+        
+        return max(up, down)
