ec6b08a1809cb74c8f9040e61513f65e404e9ca9
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-07-24 01:50:26 +0530

    Create 24 July 2024 Check for BST.py

---
+++ 24 July 2024 Check for BST.py
@@ -0,0 +1,13 @@
+class Solution:
+    
+    #Function to check whether a Binary Tree is BST or not.
+    def isBST(self, root):
+        #code here
+        def is_valid_bst(node, low, high):
+            if not node:
+                return True
+            if node.data <= low or node.data >= high:
+                return False
+            return is_valid_bst(node.left, low, node.data) and is_valid_bst(node.right, node.data, high)
+        
+        return is_valid_bst(root, float('-inf'), float('inf'))
