0c749298e30a3b12691d9009b08fe819370c30f1
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-08-07 19:58:41 +0530

    Create 7 August 2024  K-th element of two Arrays.py

---
+++ 7 August2024  K-th element of two Arrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def kthElement(self, k, arr1, arr2):
+        n, m = len(arr1), len(arr2)
+        
+        if n > m:
+            return self.kthElement(k, arr2, arr1)
+        
+        low, high = max(0, k - m), min(k, n)
+        
+        while low <= high:
+            cut1 = (low + high) // 2
+            cut2 = k - cut1
+            
+            l1 = float('-inf') if cut1 == 0 else arr1[cut1 - 1]
+            l2 = float('-inf') if cut2 == 0 else arr2[cut2 - 1]
+            r1 = float('inf') if cut1 == n else arr1[cut1]
+            r2 = float('inf') if cut2 == m else arr2[cut2]
+            
+            if l1 <= r2 and l2 <= r1:
+                return max(l1, l2)
+            elif l1 > r2:
+                high = cut1 - 1
+            else:
+                low = cut1 + 1
+                
+        return -1
