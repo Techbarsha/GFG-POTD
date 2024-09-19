1a2104dc0dc9e6228db19b2407e57f205762121b
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-09-19 20:01:04 +0530

    Create 19 September 2024 Reverse Words.py

---
+++ 19 September 2024 Reverse Words.py
@@ -0,0 +1,10 @@
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,str):
+        # code edutech barsha 
+        words = s.split('.')
+        
+        words.reverse()
+        
+        return '.'.join(words)
