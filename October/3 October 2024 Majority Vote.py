3b9cc1a6850c391446c33f0da051e8d3e950afd7
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-10-03 21:15:23 +0530

    Create 3 October 2024 Majority Vote.py

---
+++ October/3 October 2024 Majority Vote.py
@@ -0,0 +1,35 @@
+def findMajority(self, nums):
+        #Your Code goes here.
+        if not nums:
+            return [-1]
+        
+        candidate1, candidate2, count1, count2 = None, None, 0, 0
+        
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        count1, count2 = 0, 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+        
+        result = []
+        n = len(nums)
+        if count1 > n // 3:
+            result.append(candidate1)
+        if count2 > n // 3:
+            result.append(candidate2)
+        
+        return result if result else [-1]
