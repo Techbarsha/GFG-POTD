00163c3174fb5e8b4289d07facc396048c0ab2e2
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-11-19 23:42:27 +0530

    Create DAY5_Next Permutation.py

---
+++ GFG160 CHALLENGE/DAY5_Next Permutation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextPermutation(self, arr):
+        # code here
+        n = len(arr)
+        i = n - 2
+        while i >= 0 and arr[i] >= arr[i + 1]:
+            i -= 1
+        if i >= 0:  
+            j = n - 1
+            while arr[j] <= arr[i]:
+                j -= 1
+            arr[i], arr[j] = arr[j], arr[i]
+        
+        arr[i + 1:] = reversed(arr[i + 1:])
