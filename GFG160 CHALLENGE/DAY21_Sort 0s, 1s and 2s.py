e34cf685400770eeedd864763de9e3ca6a236f74
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-12-05 22:39:12 +0530

    Create DAY21_Sort 0s, 1s and 2s.py

---
+++ GFG160 CHALLENGE/DAY21_Sort 0s, 1s and 2s.py
@@ -0,0 +1,15 @@
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        # code here
+        low, mid, high = 0, 0, len(arr) - 1
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:  # arr[mid] == 2
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
