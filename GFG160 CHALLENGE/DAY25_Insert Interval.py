55796ff21c17b1e066cba8095e23460901f195ea
Barsha <122685155+Techbarsha@users.noreply.github.com>
2024-12-09 23:25:32 +0530

    Create DAY25_Insert Interval.py

---
+++ GFG160 CHALLENGE/DAY25_Insert Interval.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        # Code here
+        
+        result = []
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            else:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+        result.append(newInterval)
+        return result
