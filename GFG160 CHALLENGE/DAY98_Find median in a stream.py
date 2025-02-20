0832a6cec8c0e107890fc86a16f5d11402b16c7b
Barsha <122685155+Techbarsha@users.noreply.github.com>
2025-02-20 22:09:21 +0530

    Create DAY98_Find median in a stream.py

---
+++ GFG160 CHALLENGE/DAY98_Find median in a stream.py
@@ -0,0 +1,19 @@
+import heapq
+class Solution:
+    def getMedian(self, arr):
+        
+        ret,up,dn = [],[],[]
+        
+        for ve in arr:
+            ve = heapq.heappushpop(up,ve)
+            ve = - heapq.heappushpop(dn,-ve)
+            if len(up) == len(dn):
+                ret.append(ve)
+                heapq.heappush(up,ve)
+            elif len(up)>len(dn):
+                ret.append((up[0]+ve)/2)
+                heapq.heappush(dn,-ve)
+            else:
+                ret.append((-dn[0]+ve)/2)
+                heapq.heappush(up,ve)
+        return ret   
