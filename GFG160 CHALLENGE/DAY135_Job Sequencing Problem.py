969f2ec7e1617d9bcb33d3781cf85cd1a4c4cfa5
Barsha <122685155+Techbarsha@users.noreply.github.com>
2025-03-29 22:05:54 +0530

    Create DAY135_Job Sequencing Problem.py

---
+++ GFG160 CHALLENGE/DAY135_Job Sequencing Problem.py
@@ -0,0 +1,14 @@
+from heapq import heappush,heappop
+class Solution:
+    def jobSequencing(self, d, p):
+        # code here
+        jobs = sorted(zip(d,p))
+        pq=[]
+        for deadline,profit in jobs:
+            if len(pq)<deadline:
+                heappush(pq,profit)
+            elif pq and pq[0] < profit:
+                heappop(pq)
+                heappush(pq,profit)
+        return [len(pq),sum(pq)]        
+        
