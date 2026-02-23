62948947757dd833f82f990c8336f070b775a697
Barsha <122685155+Techbarsha@users.noreply.github.com>
2026-02-23 16:03:28 +0530

    Create Day5_Union of Arrays with Duplicates.py

---
+++ geekstreak60_2026/Day5_Union of Arrays with Duplicates.py
@@ -0,0 +1,8 @@
+class Solution:    
+    def findUnion(self, a, b):
+        # code here
+        
+        # This code implemented by barsha saha
+        
+        return list(set(a) | set(b))
+        
