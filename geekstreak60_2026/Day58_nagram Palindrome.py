be88b173e96ecfc89110338a057f705b421e00a7
Barsha <122685155+Techbarsha@users.noreply.github.com>
2026-04-17 14:05:34 +0530

    Create Day58_nagram Palindrome.py

---
+++ geekstreak60_2026/Day58_nagram Palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canFormPalindrome(self, s):
+        # code here
+        # This code implemented by Barsha Saha
+        freq = [0] * 26
+        
+        # Count frequencies
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+        odd_count = 0
+        for count in freq:
+            if count % 2 != 0:
+                odd_count += 1
+        
+        
+        return odd_count <= 1
+        
